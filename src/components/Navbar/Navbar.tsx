d3f22369fa3ce4840dace83ec2d6d9eeb7088d62
Denilson <dlemus@harding.edu>
2023-01-17 20:16:17 -0600

    ✨ Add navbar avatar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,10 @@
 import SearchIcon from "@mui/icons-material/Search";
 import HomeIcon from "@mui/icons-material/Home";
 import PeopleIcon from "@mui/icons-material/People";
-import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
-import TextsmsIcon from '@mui/icons-material/Textsms';
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import TextsmsIcon from "@mui/icons-material/Textsms";
 import NavbarOption from "../NavbarOption/NavbarOption";
-import NotificationsIcon from '@mui/icons-material/Notifications';
+import NotificationsIcon from "@mui/icons-material/Notifications";
 import "./Navbar.css";
 
 function Navbar() {
@@ -26,6 +26,10 @@ function Navbar() {
         <NavbarOption Icon={BusinessCenterIcon} title="Jobs" />
         <NavbarOption Icon={TextsmsIcon} title="Messaging" />
         <NavbarOption Icon={NotificationsIcon} title="Notifications" />
+        <NavbarOption
+          avatar="https://cdn-icons-png.flaticon.com/128/3177/3177440.png"
+          title="Me"
+        />
       </div>
     </nav>
   );
--- src/components/NavbarOption/NavbarOption.tsx
+++ src/components/NavbarOption/NavbarOption.tsx
@@ -1,14 +1,18 @@
-import "./NavbarOption.css"
+import { Avatar } from "@mui/material";
+import { SvgIconComponent } from "@mui/icons-material";
+import "./NavbarOption.css";
 
 type NavbarOptionProps = {
-  Icon: any;
+  avatar?: any;
+  Icon?: SvgIconComponent;
   title: string;
 };
 
-function NavbarOption({ Icon, title }: NavbarOptionProps) {
+function NavbarOption({ avatar, Icon, title }: NavbarOptionProps) {
   return (
     <div className="navbar-option">
       {Icon && <Icon className="navbar-option__icon" />}
+      {avatar && <Avatar className="navbar-option__icon" src={avatar} />}
       <h3 className="navbar-option__title">{title}</h3>
     </div>
   );
