811e46476a5b48b938af8c42a1d4f7083241f928
Denilson <dlemus@harding.edu>
2023-01-18 12:56:44 -0600

    💄 Update app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import './App.css'
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      App
+      <Navbar />
+      <div className="app__body">
+        <Sidebar />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
