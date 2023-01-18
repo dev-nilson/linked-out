fa301f27010370146699f0047eebcaddcc9683e9
Denilson <dlemus@harding.edu>
2023-01-17 19:14:25 -0600

    ✨ Add navbar left section

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import SearchIcon from "@mui/icons-material/Search";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar__left">
+        <img
+          src="https://cdn-icons-png.flaticon.com/128/3536/3536505.png"
+          alt="linked out logo"
+        />
+        <div className="navbar__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="navar__right"></div>
+    </nav>
+  );
+}
+
+export default Navbar;
