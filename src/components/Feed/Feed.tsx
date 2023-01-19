7067e0d5a3b743066a89fc142b3454a7b95ffcbc
Denilson <dlemus@harding.edu>
2023-01-19 11:12:56 -0600

    ✨ Display input options

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,4 +1,9 @@
 import CreateIcon from '@mui/icons-material/Create';
+import InputOption from '../InputOption/InputOption';
+import ImageIcon from '@mui/icons-material/Image';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import SmartDisplayIcon from '@mui/icons-material/SmartDisplay';
+import DescriptionIcon from '@mui/icons-material/Description';
 import "./Feed.css";
 
 function Feed() {
@@ -11,6 +16,12 @@ function Feed() {
             <input type="text" />
             <button type="submit">Send</button>
           </form>
+        </div>
+        <div className="feed__options">
+          <InputOption Icon={ImageIcon} title="Photo" color="378fe9" />
+          <InputOption Icon={SmartDisplayIcon} title="Video" color="5f9b41" />
+          <InputOption Icon={CalendarMonthIcon} title="Event" color="c37d16" />
+          <InputOption Icon={DescriptionIcon} title="Article" color="e16745" />
         </div>
       </div>
     </div>
