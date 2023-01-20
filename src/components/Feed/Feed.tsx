3021bb891ca654ea6c2504b013a9caebb0f4139c
Denilson <dlemus@harding.edu>
2023-01-20 15:08:08 -0600

    🚧 Write posts to database

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,19 +1,55 @@
-import CreateIcon from '@mui/icons-material/Create';
-import InputOption from '../InputOption/InputOption';
-import ImageIcon from '@mui/icons-material/Image';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import SmartDisplayIcon from '@mui/icons-material/SmartDisplay';
-import DescriptionIcon from '@mui/icons-material/Description';
+import React, { useEffect, useState } from "react";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+import { IPost } from "../../types/interfaces";
+import { db } from "../../../firebase";
+import CreateIcon from "@mui/icons-material/Create";
+import ImageIcon from "@mui/icons-material/Image";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import SmartDisplayIcon from "@mui/icons-material/SmartDisplay";
+import DescriptionIcon from "@mui/icons-material/Description";
+import InputOption from "../InputOption/InputOption";
+import Post from "../Post/Post";
 import "./Feed.css";
 
 function Feed() {
+  const [input, setInput] = useState("");
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const getPosts = async () => {
+  };
+
+  const sendPost = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    addDoc(collection(db, "posts"), {
+      name: "Denilson Lemus",
+      description: "Developer",
+      message: input,
+      image: "",
+      timestamp: serverTimestamp(),
+    });
+  };
+
   return (
     <div className="feed">
       <div className="feed__create">
         <div className="feed__input">
           <CreateIcon />
-          <form>
-            <input type="text" />
+          <form onSubmit={sendPost}>
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
             <button type="submit">Send</button>
           </form>
         </div>
@@ -24,8 +60,11 @@ function Feed() {
           <InputOption Icon={DescriptionIcon} title="Article" color="e16745" />
         </div>
       </div>
+      {posts.map((post: IPost) => (
+        <Post {...post} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default Feed
+export default Feed;
