9cf9743c1aa81cf5b1ed8a0f2c4fca5016edce1f
Denilson <dlemus@harding.edu>
2023-01-21 01:05:06 -0600

    ✨ Create user reducer

---
+++ src/redux/user/userSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+export interface UserState {
+  user: any;
+}
+
+const initialState: UserState = {
+  user: 0,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    login: (state, action: PayloadAction<any>) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+export const selectUser = (state: UserState) => state.user.user;
+export default userSlice.reducer;
