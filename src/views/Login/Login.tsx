2dd00248693bfbf0ba18528081ffd848dba49da0
Denilson <dlemus@harding.edu>
2023-01-23 16:35:43 -0600

    ✨ Add login functionality

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,13 +1,47 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../../redux/user/userSlice";
 import "./Login.css";
 
 function Login() {
+  const [name, setName] = useState("");
+  const [headline, setHeadline] = useState("");
+  const dispatch = useDispatch();
+
+  const register = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!headline) return alert("All fields are required");
+
+    dispatch(
+      login({
+        name: name ? name : "Anonymous",
+        headline,
+      })
+    );
+  };
+
   return (
     <div className="login">
-      <form>
-        <h1>Sign in</h1>
-        <p>Stay updated on your professional world</p>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+      <form onSubmit={register}>
+        <h1>Log in</h1>
+        <p>
+          Stay updated on your professional world
+        </p>
+        <label>Name (optional)</label>
+        <input
+          type="text"
+          placeholder="Denilson Lemus"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label>Headline*</label>
+        <input
+          type="text"
+          placeholder="Software Developer"
+          value={headline}
+          onChange={(e) => setHeadline(e.target.value)}
+        />
         <button>Sign in</button>
       </form>
     </div>
