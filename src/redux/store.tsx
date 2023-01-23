4807ce35519e9c102c3558bf6f7b1a247ece8f1d
Denilson <dlemus@harding.edu>
2023-01-23 16:44:04 -0600

    ✨ Configure persist functionality

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,11 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "./user/userSlice";
+
+const persistConfig = {
+  key: "secret",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, userReducer)
 
 export const store = configureStore({
   reducer: {
-    user: userReducer,
+    user: persistedReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store)
